38aa952b4e148b8badd52d49876a8acb2955e947
achmadfadliiskandar <af137357@gmail.com>
2021-05-14 10:43:33 +0700

    POSPHP

--- keranjang_act.php
+++ keranjang_act.php
@@ -3,36 +3,41 @@
 include 'koneksi.php';
 include 'authcheckkasir.php';
 
-// bertujuan untuk menghilangkan semua error
-error_reporting(0);
-
-var_dump("welcome to layout server ");
-echo "<a href='kasir.php'>back to kasir</a>";
-
 if (isset($_POST['id_barang'])) {
     $id_barang = $_POST['id_barang'];
-    // $qty = $_POST['qty'];
-    $qty =   1;
+    $qty = $_POST['qty'];
+    // $qty =   1;
     
     $data = mysqli_query($connect,"SELECT * FROM barang WHERE id_barang='$id_barang'");
     $b = mysqli_fetch_assoc($data);
 
-    $key_id = array_search($b['id_barang'],array_column($_SESSION['cart'],'id'));
+    // berfungsi untuk mencari array column
+    // $key_id = array_search($b['id_barang'],array_column($_SESSION['cart'],'id'));
 
-    if ($key_id !== false) {
-        $cekqty = $_SESSION['cart'][$key_id]['qty'];
-        $_SESSION['cart'][$key_id]['qty'] = $cekqty + 1;
-    }
-    else {
+    // berfungsi untuk mencari ketika array di input/klik menjadi 2
+    // if ($key_id !== false) {
+    //     $cekqty = $_SESSION['cart'][$key_id]['qty'];
+    //     $_SESSION['cart'][$key_id]['qty'] = $cekqty + 1;
+    // }
+    // output barangnya
+    // else {
+    // $barang = [
+    // 'id'=>$b['id_barang'],
+    // 'nama'=>$b['nama'],
+    // 'harga'=>$b['harga'],
+    // 'qty'=> $qty,
+    // ];
+    // $_SESSION['cart'][]= $barang;
+    // krsort berfungsi untuk mengurutkan jadi yang baru di atas
+    // // krsort($_SESSION['cart']);
+    // }
     $barang = [
     'id'=>$b['id_barang'],
     'nama'=>$b['nama'],
     'harga'=>$b['harga'],
-    'qty'=> $qty,
+    'qty'=> 1,
     ];
     $_SESSION['cart'][]= $barang;
-    // krsort($_SESSION['cart']);
-    }
-        header("Location:kasir.php");
+    header("Location:kasir.php");
     }
 ?>
